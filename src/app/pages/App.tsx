d1624daf328c2b3ee7406cd2297d738be8557e84
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2019-11-15 08:03:12 +0000

    add bootstrap 4+

--- src/app/pages/App.tsx
+++ src/app/pages/App.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../logo.svg";
 import "./App.scss";
+import { Button, Modal } from "react-bootstrap";
 
 const App: React.FC = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +25,57 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
+
+      <div className="p-1">
+        <Button variant="primary" className="mr-1">
+          Primary
+        </Button>
+        <Button variant="secondary" className="mr-1">
+          Secondary
+        </Button>
+        <Button variant="success" className="mr-1">
+          Success
+        </Button>
+        <Button variant="warning" className="mr-1">
+          Warning
+        </Button>
+        <Button variant="danger" className="mr-1">
+          Danger
+        </Button>
+        <Button variant="info" className="mr-1">
+          Info
+        </Button>
+        <Button variant="light" className="mr-1">
+          Light
+        </Button>
+        <Button variant="dark" className="mr-1">
+          Dark
+        </Button>
+        <Button variant="link" className="mr-1">
+          Link
+        </Button>
+      </div>
+
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     </div>
   );
 };
