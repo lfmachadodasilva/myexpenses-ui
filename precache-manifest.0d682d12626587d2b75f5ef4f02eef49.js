fead6c91fdf7627ede2309905e252fe78b113596
lfmachadodasilva <lfmachadodasilva@gmail.com>
2020-07-02 13:26:17 +0000

    Deploying to gh-pages from  @ 947ed60554cfc0a60f005e1d21bdd56b089cb25e 🚀

---
+++ precache-manifest.0d682d12626587d2b75f5ef4f02eef49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d45a6744ee68e61ef2ed6946c7bde07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "925bd0a21d768e5a722e",
+    "url": "./static/css/main.843892a3.chunk.css"
+  },
+  {
+    "revision": "2130686afd0bed88b62f",
+    "url": "./static/js/2.dea206ac.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "./static/js/2.dea206ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "925bd0a21d768e5a722e",
+    "url": "./static/js/main.f7129723.chunk.js"
+  },
+  {
+    "revision": "259e2a5392cf17ed104a",
+    "url": "./static/js/runtime-main.955116fa.js"
+  }
+]);
