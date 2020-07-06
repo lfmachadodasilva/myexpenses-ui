fefa7f490ae1209a2f022a80d47d3a01a3400493
lfmachadodasilva <lfmachadodasilva@gmail.com>
2020-07-06 10:21:20 +0000

    Deploying to gh-pages from  @ f5ab57b66512e703305f92b10e64732bd6818743 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myexpenses-ui/precache-manifest.405ff0d671b0dd5b90dd1a9dec670530.js"
+  "/myexpenses-ui/precache-manifest.41897b45f72217ea52b241f17edf1eb5.js"
 );
 
 self.addEventListener('message', (event) => {
