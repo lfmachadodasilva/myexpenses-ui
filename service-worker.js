b4f58d644b4827478bc594a84248a30af46ff6df
lfmachadodasilva <lfmachadodasilva@gmail.com>
2020-10-28 10:59:07 +0000

    Deploying to gh-pages from  @ 787d620dddd2c1a6a6a28d409db74d176a21e197 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3046965c29c1b44f03e3bfd5c1a154cf.js"
+  "/myexpenses-ui/precache-manifest.96aefc09dbf865bbb64265ab87dc8c6b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myexpenses-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
