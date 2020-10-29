e2c8d0265a44c29d057b0087f48f826bb6aefbed
lfmachadodasilva <lfmachadodasilva@gmail.com>
2020-10-29 15:59:03 +0000

    Deploying to gh-pages from  @ 0102463ba03617d33228a7eeb50d5d02506686e7 🚀

---
+++ precache-manifest.b8cd438a1d6f2365fef3a3917db7f985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175ff96254b6e1160f93554e2bf583f9",
+    "url": "/myexpenses-ui/index.html"
+  },
+  {
+    "revision": "218dd010710d3a751a2a",
+    "url": "/myexpenses-ui/static/css/main.cabb26f2.chunk.css"
+  },
+  {
+    "revision": "50819a04234fd78f70d1",
+    "url": "/myexpenses-ui/static/js/2.c11e5313.chunk.js"
+  },
+  {
+    "revision": "7821345a4c59d207bdb0a8564225fba0",
+    "url": "/myexpenses-ui/static/js/2.c11e5313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218dd010710d3a751a2a",
+    "url": "/myexpenses-ui/static/js/main.d746a227.chunk.js"
+  },
+  {
+    "revision": "53a8ce50f4b39982e510",
+    "url": "/myexpenses-ui/static/js/runtime-main.342f53ef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myexpenses-ui/precache-manifest.ce5c90ab6507dede4954a719aebde61f.js"
+  "/myexpenses-ui/precache-manifest.b8cd438a1d6f2365fef3a3917db7f985.js"
 );
 
 self.addEventListener('message', (event) => {
